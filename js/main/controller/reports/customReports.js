8c22e45cc94793739b5fd2b4cebc0ab1ae655658
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2017-12-14 16:55:10 +0200

    create update on preivew custom reports

--- js/main/controller/reports/customReports.js
+++ js/main/controller/reports/customReports.js
@@ -2,6 +2,8 @@
     function customReports($rootScope, $scope, Vacancy, Service, $location, $routeParams, notificationService, $filter, translateWords,
                            $translate, vacancyStages, Stat, Company, vacancyStages, Person, $uibModal, CustomReportsService) {
         try {
+            let updateReport = () => CustomReportsService.buildReport.call(this, $scope);
+
             this.dataReport        = CustomReportsService.data;
             this.inHover           = CustomReportsService.inHover;
             this.outHover          = CustomReportsService.outHover;
@@ -13,11 +15,11 @@
             this.closeModal        = CustomReportsService.closeModal;
             this.timeMaxZone       = CustomReportsService.timeMaxZone;
             this.timeMaxZone2      = CustomReportsService.timeMaxZone2;
+            this.updateReport      = updateReport;
             CustomReportsService.vacancyStages.call(this);
             CustomReportsService.getAllPersons.call(this);
-            CustomReportsService.getDate.call(this, this.dataReport);
+            CustomReportsService.getDate.call(this, this.dataReport, $scope);
             CustomReportsService.buildReport.call(this, $scope);
-
         }catch(erorr){
             console.log(erorr, 'erorr CustomReports')
         }
