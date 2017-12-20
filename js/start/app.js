fc9ac9b1bfd129e25bf89a917ab2ad4a92f61821
Ivan Romanenko <electrik64@gmail.com>
2017-12-20 16:36:03 +0200

    fixed

--- js/start/app.js
+++ js/start/app.js
@@ -90,7 +90,7 @@ var app = angular.module('RecruitingAppStart', [
 }]).config(function($translateProvider,tmhDynamicLocaleProvider) {
     $translateProvider.useStaticFilesLoader({
         prefix: 'languange/locale-',
-        suffix: '.json?b=5'
+        suffix: '.json?b=6'
     });
     $translateProvider.translations('en');
     $translateProvider.translations('ru');
