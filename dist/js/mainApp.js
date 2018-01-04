455b3ba6f4d1dfeca48d66d0154e8a87583d319e
Ivan Romanenko <electrik64@gmail.com>
2018-01-04 11:13:35 +0200

    done

--- dist/js/mainApp.js
+++ dist/js/mainApp.js
@@ -29760,7 +29760,7 @@ function navBarController($q, Vacancy, serverAddress, notificationService, $scop
                     });
                 }
 
-                if($rootScope.me.recrutRole == 'admin') {
+                // if($rootScope.me.recrutRole == 'admin') {
                     Account.getAccountInfo(function(resp){
                         if(resp.status != 'error'){
                             if(resp.object && resp.object.tillDate) {
@@ -29933,7 +29933,7 @@ function navBarController($q, Vacancy, serverAddress, notificationService, $scop
                         }
                         //////////////
                     });
-                }
+                // }
 
                 if($rootScope.modalInstance){
                     $rootScope.modalInstance.closed.then(function(){
