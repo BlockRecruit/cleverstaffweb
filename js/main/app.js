249dbf4dbe88a51fabfe97cde1b46413037e6df3
Ivan Romanenko <electrik64@gmail.com>
2018-01-04 09:33:08 +0200

    versions

--- js/main/app.js
+++ js/main/app.js
@@ -670,7 +670,7 @@ angular.module('RecruitingApp', [
     /************************************/
     $translateProvider.useStaticFilesLoader({
         prefix: 'languange/locale-',
-        suffix: '.json?b=38'
+        suffix: '.json?b=39'
     });
     $translateProvider.translations('en');
     $translateProvider.translations('ru');
