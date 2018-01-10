c9f468093dcc8ea39a780b10e095bddabc1dff7e
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2018-01-10 14:22:04 +0200

    fix error in CS reports

--- js/main/service/statService.js
+++ js/main/service/statService.js
@@ -1,7 +1,7 @@
 angular.module('services.reportAll', [
     'ngResource',
     'ngCookies'
-]).factory('Stat', ['$resource', 'serverAddress', '$filter', '$localStorage','$rootScope', function($resource, serverAddress, $filter, $localStorage, $rootScope) {
+]).factory('Stat', ['$resource', 'serverAddress', '$filter', '$localStorage','$rootScope','notificationService', function($resource, serverAddress, $filter, $localStorage, $rootScope, notificationService) {
 
     var stat = $resource(serverAddress + '/stat/:param', {param: "@param"}
         , {
@@ -111,27 +111,34 @@ angular.module('services.reportAll', [
                 stat.getCustomVacancyReports(resp => resolve(resp),error => reject(error));
             });
         };
+
         stat.requestGetActualVacancyStatistic2 = function (params, update){
             $rootScope.loading = true;
             return new Promise((resolve, reject) => {
                 if(update && data) return resolve(data);
                 stat.getActualVacancyStatistic2(params, resp => {
-                    if(resp.object.entryList && resp.object.entryList.length > 0){
-                        data = resp;
-                        resolve(resp, resp['request'] = 'Statistic2');
+                    if(resp.status == 'error'){
+                        notificationService.error(resp.message);
+                        $rootScope.loading = false;
                     }else{
-                        resp.object.entryList = [];
-                        resolve(resp, resp['request'] = 'Statistic2');
+                        if(resp.object.entryList && resp.object.entryList.length > 0){
+                            data = resp;
+                            resolve(resp, resp['request'] = 'Statistic2');
+                        }else{
+                            resp.object.entryList = [];
+                            resolve(resp, resp['request'] = 'Statistic2');
+                        }
                     }
                 },error => reject(error));
             });
         };
-        stat.requestDeleteCustomVacancyReport = function (params){
-            $rootScope.loading = true;
-            return new Promise((resolve, reject) => {
-                stat.deleteCustomVacancyReport(params, resp => resolve(resp),error => reject(error));
-            });
-        };
+
+    stat.requestDeleteCustomVacancyReport = function (params){
+        $rootScope.loading = true;
+        return new Promise((resolve, reject) => {
+            stat.deleteCustomVacancyReport(params, resp => resolve(resp),error => reject(error));
+        });
+    };
     stat.requestGetCountVacancyForActualVacancyStatistic = function (params){
         $rootScope.loading = true;
         return new Promise((resolve, reject) => {
