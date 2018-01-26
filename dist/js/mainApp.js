dcb8832dcb0e58daef24e2815892665ee0b4e3f7
Ivan Romanenko <electrik64@gmail.com>
2018-01-26 18:16:56 +0200

    fixed

--- dist/js/mainApp.js
+++ dist/js/mainApp.js
@@ -40058,7 +40058,7 @@ function historyButton($scope, resp, Service, CacheCandidates) {
             }
         });
     };
-    $scope.getLastEvent = function(count) {z
+    $scope.getLastEvent = function(count) {
         if (!count) {
             count = 1;
         }
