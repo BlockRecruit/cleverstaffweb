473efb599072bd06ea68f7ddc92dbd555682edd2
Viktor Shevchenko <thebestsheva@ukr.net>
2018-02-13 09:26:07 +0200

    work on task

--- dist/js/mainApp.js
+++ dist/js/mainApp.js
@@ -21595,6 +21595,9 @@ controller.controller('CandidateEditController', ["$http", "$rootScope", "$scope
         };
 
         $scope.selectFavoriteContacts = function ($scope, type, event) {
+            console.log($scope);
+            console.log(type);
+            console.log(event);
             Candidate.setSelectFavoriteContacts($scope, type, event );
         };
 
