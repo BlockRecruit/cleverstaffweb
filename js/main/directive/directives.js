8cd50fbf3133c191a5b9f0475b5d23411ab8a44b
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2018-03-16 10:41:18 +0200

    rebase

--- js/main/directive/directives.js
+++ js/main/directive/directives.js
@@ -4504,7 +4504,37 @@ directive('appVersion', ['version', function(version) {
 
             }
         }
-    }]);
+    }])
+    .directive("customSelect",setCustomSelect);
+function setCustomSelect(){
+    let restrict  = "EACM",
+        scope = {
+            data:"=",
+            model:"=",
+            placeholder:"@",
+            method:"="
+        },
+        template = `
+        <div class="select clearfix">
+            <input type="text" ng-model="model" placeholder="{{placeholder|translate}}" readonly class="form-control col-lg-12 select-input-field">
+            <div class="dropdown-content" style="z-index: -999">
+                <ul>
+                    <li ng-repeat="item in data track by $index" ng-click="method(item)" ng-class="{disable: (item.status == 'N')}">{{item.text|translate}}</li>
+                </ul>
+            </div>
+        </div>`;
+    return {
+        restrict,
+        scope,
+        template,
+        link(scope, element, attrs){
+            scope.showOrHideDropDownList = $event => {
+                console.log($event, '$event');
+            };
+        }
+    }
+};
+
 function similar_text(first, second, percent) {
     if (first === null || second === null || typeof first === 'undefined' || typeof second === 'undefined') {
         return 0;
