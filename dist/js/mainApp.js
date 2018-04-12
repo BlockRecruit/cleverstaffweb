04e20288d9e5afc436b47e0f8ca7e3515b6387d3
ValeriyTernavskiy <valeraternavckiy1994@gmail.com>
2018-04-12 13:17:50 +0300

    versions

--- dist/js/mainApp.js
+++ dist/js/mainApp.js
@@ -14984,7 +14984,7 @@ angular.module('RecruitingApp', [
     /************************************/
     $translateProvider.useStaticFilesLoader({
         prefix: 'languange/locale-',
-        suffix: '.json?b=80'
+        suffix: '.json?b=82'
     });
     $translateProvider.translations('en');
     $translateProvider.translations('ru');
