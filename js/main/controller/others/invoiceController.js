e4f44adb46882c38f60eb813b567be7cd12b1ef4
JohnInv <electrik64@gmail.com>
2018-04-19 14:59:28 +0300

    q

--- js/main/controller/others/invoiceController.js
+++ js/main/controller/others/invoiceController.js
@@ -1,36 +1,90 @@
-controller.controller('invoiceController', ['$rootScope', '$scope', 'Invoice', 'Person', 'Account', function($rootScope, $scope, Invoice, Person, Account) {
+controller.controller('invoiceController', ['$rootScope', '$scope', 'Service', 'Invoice', 'Person', 'Account', '$filter', '$uibModal',
+    function($rootScope, $scope, Service, Invoice, Person, Account, $filter, $uibModal) {
 
     $scope.allPersons = [];
     $scope.accountInfo = {};
     $scope.invoice = {};
-    $scope.customer = { address: null, country: null, city: null, company: null, companyId: null, fullName: null, position: null, postalCode: null };
+    $scope.customer = { address: null, country: null, city: null, companyName: null, companyId: null, fullName: null, position: null, postalCode: null };
+    $scope.validation = { invalidFields: [], checking: false};
 
-    // $scope.$watch('customer', () => {
-    //     $scope.
-    // }, true);
+    $scope.currenciesSigns = Invoice.getCurrenciesSigns();
+    $scope.countries = Service.getAllCounties($rootScope.currentLang);
 
     $scope.generateInvoice = function() {
-        Object.entries($scope.customer).map(([key, value]) => {
-            console.log(key, $scope.customerForm[key].$viewValue);
+        $scope.validation = { invalidFields: [], checking: true};
+        if(validatedCustomerForm()) openInvoiceConfirmModal();
+    };
+
+    $scope.downloadInvoice = function() {
+        $rootScope.loading = true;
+        Invoice.getInvoice({
+            amount : parseFloat($scope.invoice.price()),
+            customerName : $scope.customer.companyName,
+            customerRegistrationNumber  : $scope.customer.companyId,
+            customerAddress : `${$scope.customer.country},${$scope.customer.city},${$scope.customer.address},${$scope.customer.postalCode}`,
+            representerPosition : $scope.customer.position,
+            representerName : $scope.customer.fullName,
+            currencyAsString : $scope.invoice.currency.toLowerCase()
+        }).then(resp => {
+            $rootScope.loading = false;
+            console.log(resp);
+            $scope.modalInstance.close();
+        }, error => {
+            $rootScope.loading = false;
+            $scope.modalInstance.close();
+            notificationService.error(error.message)
         });
     };
+
+    $scope.$watch('customer', () => {
+        if($scope.customerForm.$valid) {
+            $scope.validation = { invalidFields: [], checking: false};
+        }
+    }, true);
+
+    function validatedCustomerForm() {
+        Object.entries($scope.customer).map(([key, value]) => {
+            if($scope.customerForm[key].$invalid) {
+                $scope.validation.invalidFields.push($filter('translate')(key));
+            }
+        });
+        console.log($scope.validation.invalidFields);
+        return !Boolean($scope.validation.invalidFields.length);
+    }
+
+    function openInvoiceConfirmModal() {
+        $scope.modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: '../partials/modal/invoice-confirm.html',
+            size: '',
+            scope: $scope,
+            resolve: function(){}
+        });
+    }
+
 
     (() => {
         $rootScope.loading = true;
         Promise.all([
             Person.requestGetAllPersons(),
-            Account.accountInfo()
-        ]).then(([allPersons, accountInfo]) => {
+            Account.accountInfo(),
+            Invoice.getCurrenciesExchangeRates(['USD', 'RUB', 'EUR'])
+        ]).then(([allPersons, accountInfo, currencyExchangeRates]) => {
+                $rootScope.loading = false;
                 $scope.allPersons = allPersons.object;
                 $scope.accountInfo = accountInfo.object;
 
+                $scope.currenciesMonthRates = Invoice.getCurrenciesMonthRates(currencyExchangeRates, accountInfo.object.monthRate);
+
                 $scope.months = [1,2,3,4,5,6,7,8,9,10,11,12];
+                $scope.currencies = ['UAH', 'RUR', 'EUR', 'USD'];
                 $scope.invoice = {
                     users: Object.keys(allPersons.object).length,
-                    month: 12,
-                    price: () => $scope.invoice.users * $scope.invoice.month * $scope.accountInfo.monthRate
+                    month: $scope.months.length,
+                    currency: 'USD',
+                    price: () => ($scope.invoice.users * $scope.invoice.month * $scope.currenciesMonthRates[$scope.invoice.currency]).toFixed(2)
                 };
-                $rootScope.loading = false;
+                $scope.$apply();
             }).catch(error => {
                 $rootScope.loading = false;
                 console.log(error);
--- js/main/service/InvoiceService.js
+++ js/main/service/InvoiceService.js
@@ -1,13 +1,69 @@
 angular.module('services.invoice', [
-        'ngResource'
+    'ngResource',
+    'ngCookies'
     ]
-).factory('Invoice', ['$resource', 'serverAddress', function($resource, serverAddress) {
-    const invoice = $resource(serverAddress + '/notice/:param', {param: "@param"},
+).factory('Invoice', ['$resource', 'serverAddress', '$http', function($resource, serverAddress, $http) {
+    const invoice = $resource(serverAddress + '/:param', {param: "@param"},
         {
+            getInv: {
+                method : "POST",
+                headers: {'Content-type':'application/json; charset=UTF-8'},
+                params: {
+                    param: 'getInvoice'
+                }
+            }
         });
 
+    invoice.getCurrenciesSigns = () => ({ EUR: '€',  USD: '$', UAH : '₴', RUR: '₽'});
 
-    invoice.test = () => 'test';
+    invoice.getInvoice = function(params) {
+        return new Promise((resolve, reject) => {
+           invoice.getInv(params, resp => {
+               if(resp.status === 'ok') {
+                   resolve(resp);
+               } else {
+                   reject(resp);
+               }
+           }, error => reject(error))
+        });
+    };
 
+    invoice.getCurrenciesExchangeRates = function(invoiceCurrencies) {
+        return new Promise ((resolve, reject) => {
+            $http.get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+                .then(response => {
+                    if(response.status === 200) resolve(filterRequiredCurrencies(response.data, invoiceCurrencies));
+                }, error => {
+                    reject(error);
+                });
+        })
+    };
+
+    invoice.getCurrenciesMonthRates = function(currencies, USDrate) {
+        let monthRates = {};
+        Object.entries(currencies).forEach(([key, value]) => {
+            monthRates[value.cc] = (USDrate * currencies['USD'].rate) / value.rate ;
+        });
+
+        monthRates['RUR'] = monthRates['RUB'];
+        return monthRates;
+    };
+
+    function filterRequiredCurrencies(allCurrencies, invoiceCurrencies) {
+        let currencies = {};
+
+        for(let i = 0; i < allCurrencies.length; i++ ) {
+            for( let j = 0; j < invoiceCurrencies.length; j++ ) {
+                if(invoiceCurrencies[j].toLowerCase() === allCurrencies[i].cc.toLowerCase()) {
+                    currencies[allCurrencies[i].cc] = allCurrencies[i];
+                }
+            }
+        }
+
+        currencies['UAH'] = { cc: 'UAH', rate: currencies['USD'].rate / currencies['USD'].rate};
+        currencies['RUR'] = currencies['RUB'];
+
+        return currencies;
+    }
     return invoice;
 }]);
