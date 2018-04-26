93f4c54a960ecb5c6741f2518823a665bbdb45e4
Viktor Shevchenko <thebestsheva@ukr.net>
2018-04-26 11:56:31 +0300

    build and versions

--- js/main/app.js
+++ js/main/app.js
@@ -679,7 +679,7 @@ angular.module('RecruitingApp', [
     /************************************/
     $translateProvider.useStaticFilesLoader({
         prefix: 'languange/locale-',
-        suffix: '.json?b=88'
+        suffix: '.json?b=89'
     });
     $translateProvider.translations('en');
     $translateProvider.translations('ru');
