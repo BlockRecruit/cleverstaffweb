5334be10d43d3380a64c5b221f6be562730468ab
Pokyntelytsia <pokintelica@ukr.net>
2018-05-08 10:44:59 +0300

    changed
    versions

--- dist/js/startApp.js
+++ dist/js/startApp.js
@@ -8403,7 +8403,7 @@ angular.module('services.globalService', [
         }
 
         function setCookies(key, value) {
-            document.cookie = key + "=" + value;
+            document.cookie = key + "=" + value + "; expires=Thu, 18 Dec 2033 12:00:00 UTC";
         }
     };
 
