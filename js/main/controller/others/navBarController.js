85c6cde05c63659ba8812a361f696e571a76670c
JohnInv <electrik64@gmail.com>
2018-05-11 13:11:27 +0300

    done

--- js/main/controller/others/navBarController.js
+++ js/main/controller/others/navBarController.js
@@ -16,7 +16,8 @@ function navBarController($q, Vacancy, serverAddress, notificationService, $scop
     $scope.readedNews = [];
     $scope.bonus = 0;
     $scope.paidUsers = [];
-    $scope.months = [{label:1, value:1},{label:2, value:2},{label:3, value:3},{label:4, value:4},{label:5, value:5},{label:6, value:6},{label:7, value:7},{label:8, value:8},{label:9, value:9},{label:10, value:10},{label:11, value:11},{label:12, value:12}];
+    // $scope.months = [{label:1, value:1},{label:2, value:2},{label:3, value:3},{label:4, value:4},{label:5, value:5},{label:6, value:6},{label:7, value:7},{label:8, value:8},{label:9, value:9},{label:10, value:10},{label:11, value:11},{label:12, value:12}];
+    $scope.months = [1,2,3,4,5,6,7,8,9,10,11,12];
     $scope.countPeople = 0;
     $scope.countMonth = 4;
     //localStorage.setItem("readedNews", '');
@@ -1151,13 +1152,14 @@ function navBarController($q, Vacancy, serverAddress, notificationService, $scop
         Person.getAllPersons((resp) => {
             angular.forEach(resp.object, function (val) {
                 if (val.status == "A" && val.recrutRole != 'client') {
-                    $scope.paidUsers.push({label: $scope.paidUsers.length + 1, value: $scope.paidUsers.length + 1});
+                    // $scope.paidUsers.push({label: $scope.paidUsers.length + 1, value: $scope.paidUsers.length + 1});
+                    $scope.paidUsers.push($scope.paidUsers.length + 1);
                 }
             });
             if($rootScope.blockUserData && $rootScope.blockUserData.payment_min_users > $scope.paidUsers.length) {
                 const diff = $rootScope.blockUserData.payment_min_users - $scope.paidUsers.length;
                 for(let i = 0; i < diff + 2; i++) {
-                    $scope.paidUsers.push({label: $scope.paidUsers.length + 1, value: $scope.paidUsers.length + 1});
+                    $scope.paidUsers.push($scope.paidUsers.length + 1);
                 }
             }
             $scope.countPeople = $scope.paidUsers.length;
